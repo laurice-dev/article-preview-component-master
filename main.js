a89a18fc47d4c9492849628056a38aa8aede595c
Laurice <laurice.medley@gmail.com>
2025-06-03 10:37:16 -0400

    fix: added onClick background to button and added drop-shadows

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var button = document.querySelector(".card__social-button");
+var socialButton = document.querySelector(".card__social-button");
 var socialLinks = document.querySelector(".card__social-popup--in-active");
 
-button.addEventListener("click", function () {
+socialButton.addEventListener("click", function () {
   socialLinks.classList.toggle("card__social-popup--active");
+  socialButton.classList.toggle("card__social-button--dark");
 });
